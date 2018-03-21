0e4663293de5046dab6e7c107889f5f7592e8744
raccoonmaster <32940684+raccoonmaster@users.noreply.github.com>
2018-03-21 15:52:42 +0100

    Java project

---
+++ javacv/src/Main.java
@@ -0,0 +1,154 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.awt.image.WritableRaster;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import java.io.File;
+import java.net.URL;
+import org.bytedeco.javacv.*;
+import org.bytedeco.javacpp.*;
+import org.bytedeco.javacpp.indexer.*;
+import static org.bytedeco.javacpp.opencv_core.*;
+import static org.bytedeco.javacpp.opencv_imgproc.*;
+import static org.bytedeco.javacpp.opencv_calib3d.*;
+import static org.bytedeco.javacpp.opencv_objdetect.*;
+
+
+
+public class JPanelOpenCV extends JPanel{
+
+    BufferedImage image;
+
+    public static void main (String args[]) throws InterruptedException{
+        //System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+
+        JPanelOpenCV t = new JPanelOpenCV();
+        opencv_videoio.VideoCapture camera = new opencv_videoio.VideoCapture(0);
+
+        Mat frame = new Mat();
+        camera.read(frame);
+
+        if(!camera.isOpened()){
+            System.out.println("Error");
+        }
+        else {
+            while(true){
+
+                if (camera.read(frame)){
+
+                    BufferedImage image = t.MatToBufferedImage(frame);
+
+                    t.window(image, "Original Image", 0, 0);
+
+                    t.window(t.grayscale(image), "Processed Image", 40, 60);
+
+                    //t.window(t.loadImage("ImageName"), "Image loaded", 0, 0);
+
+                    break;
+                }
+            }
+        }
+        camera.release();
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        g.drawImage(image, 0, 0, this);
+    }
+
+    public JPanelOpenCV() {
+    }
+
+    public JPanelOpenCV(BufferedImage img) {
+        image = img;
+    }
+
+    //Show image on window
+    public void window(BufferedImage img, String text, int x, int y) {
+        JFrame frame0 = new JFrame();
+        frame0.getContentPane().add(new JPanelOpenCV(img));
+        frame0.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame0.setTitle(text);
+        frame0.setSize(img.getWidth(), img.getHeight() + 30);
+        frame0.setLocation(x, y);
+        frame0.setVisible(true);
+    }
+
+    //Load an image
+    public BufferedImage loadImage(String file) {
+        BufferedImage img;
+
+        try {
+            File input = new File(file);
+            img = ImageIO.read(input);
+
+            return img;
+        } catch (Exception e) {
+            System.out.println("erro");
+        }
+
+        return null;
+    }
+
+    //Save an image
+    public void saveImage(BufferedImage img) {
+        try {
+            File outputfile = new File("Images/new.png");
+            ImageIO.write(img, "png", outputfile);
+        } catch (Exception e) {
+            System.out.println("error");
+        }
+    }
+
+    //Grayscale filter
+    public BufferedImage grayscale(BufferedImage img) {
+        for (int i = 0; i < img.getHeight(); i++) {
+            for (int j = 0; j < img.getWidth(); j++) {
+                Color c = new Color(img.getRGB(j, i));
+
+                int red = (int) (c.getRed() * 0.299);
+                int green = (int) (c.getGreen() * 0.587);
+                int blue = (int) (c.getBlue() * 0.114);
+
+                Color newColor =
+                        new Color(
+                                red + green + blue,
+                                red + green + blue,
+                                red + green + blue);
+
+                img.setRGB(j, i, newColor.getRGB());
+            }
+        }
+
+        return img;
+    }
+
+    public BufferedImage MatToBufferedImage(Mat frame) {
+        //Mat() to BufferedImage
+        int type = 0;
+        if (frame.channels() == 1) {
+            type = BufferedImage.TYPE_BYTE_GRAY;
+        } else if (frame.channels() == 3) {
+            type = BufferedImage.TYPE_3BYTE_BGR;
+        }
+        BufferedImage image = new BufferedImage(frame.width(), frame.height(), type);
+        WritableRaster raster = image.getRaster();
+        DataBufferByte dataBuffer = (DataBufferByte) raster.getDataBuffer();
+        byte[] data = dataBuffer.getData();
+        frame.get(0, 0, data);
+
+        return image;
+    }
+
+}
